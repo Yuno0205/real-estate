c1a520a3efe36f4fa546fe3e9616f5442543d09b
Yuno0205 <mainhathao195@gmail.com>
2024-11-08 15:19:44 +0700

    update

--- app/components/SaiGonRiver/index.tsx
+++ app/components/SaiGonRiver/index.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
-import img from "@/public/static/images/7-TANG-BLUE-FOREST-1024x1024.jpg";
+import img from "@/public/static/images/villa.jpg";
 import {
   Aguafina_Script,
   Noto_Serif_Toto,
   Prata,
   Oswald,
+  Montserrat,
+  Inter,
 } from "next/font/google";
 import clsx from "clsx";
+
+const inter = Inter({
+  subsets: ["vietnamese", "latin"],
+  weight: ["400", "700"],
+});
 
 const aguafina = Aguafina_Script({
   subsets: ["latin"],
@@ -28,102 +35,131 @@ const oswald = Oswald({
   weight: ["400"],
 });
 
+const mont = Montserrat({
+  subsets: ["latin", "vietnamese"],
+  weight: ["400", "700"],
+});
+
 export default function SaiGonRiver() {
   return (
-    <div className="w-full h-full pt-[167px] pb-[200px] flex px-3">
-      <div className="w-2/5 text-center">
-        <div className="w-full">
-          <div className="h-[1px] w-1/12 align-middle bg-[#bcbcbc] inline-block mb-4"></div>
-          <span className={clsx("text-4xl text-[#185822]", aguafina.className)}>
-            Ecovillage Saigon River
-          </span>
-        </div>
-        <div className={clsx("w-full text-justify py-5", noto.className)}>
-          <div className="w-11/12">
-            <p className="mb-6">
-              EcoVillage Sai Gon River sở hữu 3 mặt giáp sông đắt giá, quy mô
-              55ha nhưng mật độ xây dựng chỉ 15%. Đây là Dự án biểu tượng đầu
-              tiên của Nhà sáng lập Ecopark tại miền Nam
-            </p>
-            <p className="mb-6">
-              Tại EcoVillage Saigon với mật độ cây xanh lên đến 170 cây/người,
-              bao bọc bởi 48.5ha cây xanh và mặt nước. Tổng số lượng thương phẩm
-              chỉ 668 sản phẩm (Nhà phố & Biệt thự) cực kỳ giới hạn, và 1 Khu
-              Resorts 6 sao mang thương hiệu lẫy lừng thế giới Kempinskin đậm
-              chất nghĩ dưỡng
-            </p>
-          </div>
-        </div>
-
-        <div className="w-11/12 mt-5">
-          <div className="text-justify font-medium leading-6 text-[#232323] border-solid border-l-4 border-[#FF7A56] py-1 pl-8">
-            <p className={clsx("mb-5 text-justify", prata.className)}>
-              Nơi vùng đất Blue Zones thứ 6 trên thế giới được đánh thức, được
-              bao quanh bởi dòng sông Sài Gòn thơ mộng
-            </p>
-            <footer
+    <div className="w-full h-full">
+      <div className="w-full max-w-[1140px] mx-auto pt-[167px] pb-[200px] flex px-3">
+        <div className="w-1/3 ">
+          <div className="w-full relative">
+            <div className="h-[1px] w-1/12 align-middle bg-[#bcbcbc] inline-block mb-4 absolute left-[-50px] top-4"></div>
+            <span
               className={clsx(
-                "font-semibold uppercase leading-5 tracking-wide text-[#FF7A56]",
-                oswald.className
+                "text-3xl font-bold text-[#185822]",
+                mont.className
               )}
             >
-              Sản phẩm của nhà sáng lập Ecopark
-            </footer>
-          </div>
-        </div>
-      </div>
-      <div className="w-1/2 flex items-center p-4">
-        <Image src={img} alt="img" className="rounded-lg object-cover" />
-      </div>
-      <div className="flex flex-1 flex-col justify-around">
-        {/* Items */}
-        <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
-          <div className="mb-2.5">
-            <span className="text-3xl text-[#2E562A] font-semibold">30</span>
-            <span className="text-xl text-[#B57C46] ml-2 translate-y-60 float-top">
-              %
+              Elite Life
+              <br /> Chuẩn cho cuộc sống ưu tú
             </span>
           </div>
-          <div className="text-[#939393]">
-            <span>CÂY XANH & MẶT NƯỚC</span>
+          <div className={clsx("w-full text-justify py-5", inter.className)}>
+            <div className="w-11/12">
+              <p className={clsx("mb-6 leading-7")}>
+                <span className="text-[#008000] font-semibold">
+                  Elite Life dự án nhà phố compound
+                </span>{" "}
+                được ví von giống như một ốc đảo xanh thu nhỏ đang tỏa sáng sát
+                vách Khu đô thị Phú Mỹ Hưng, với kiến trúc xanh những tác phẩm
+                nhà phố - biệt thự hiện đại kết hợp cùng các tiện ích nội khu đa
+                dạng và đẳng cấp.
+              </p>
+              <p className="mb-6 leading-7">
+                Tại Elite Life với mật độ không gian xanh, tiện ích và giao
+                thông lên đến &lt; 26000 m2. Tổng số lượng thương phẩm chỉ 310
+                sản phẩm (Nhà phố, Biệt thự và shophouse) cực kỳ giới hạn, và
+                độc nhất tại khu công nghiệp Long Hậu.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-11/12 mt-5">
+            <div className="text-justify font-medium leading-6 text-[#232323] border-solid border-l-4 border-[#FF7A56] py-1 pl-8">
+              <p
+                className={clsx("mb-5 text-justify leading-6", prata.className)}
+              >
+                Elite Life sẽ là nơi kiến tạo nên cộng đồng cư dân có cùng khao
+                khát về không gian sống sang trọng khác biệt, hình thành phong
+                cách sống ưu tú đúng nghĩa.
+              </p>
+              <footer
+                className={clsx(
+                  "font-semibold uppercase leading-5 tracking-wide text-[#FF7A56]",
+                  oswald.className
+                )}
+              >
+                Sản phẩm của nhà sáng lập Tân Thái Thịnh
+              </footer>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
-          <div className="mb-2.5">
-            <span className="text-3xl text-[#2E562A] font-semibold">15</span>
-            <span className="text-xl text-[#B57C46] ml-2 translate-y-60 float-top">
-              %
-            </span>
-          </div>
-          <div className="text-[#939393]">
-            <span>MẬT ĐỘ XÂY DỰNG</span>
-          </div>
+        <div className="w-1/2 flex items-center p-4">
+          <Image
+            src={img}
+            alt="img"
+            className="rounded-lg object-cover h-4/5"
+          />
         </div>
-        <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
-          <div className="mb-2.5">
-            <span className="text-3xl text-[#2E562A] font-semibold">4</span>
-            <span className="text-xl text-[#B57C46] ml-2 translate-y-60 float-top">
-              Ha
-            </span>
+        <div className="flex flex-1 flex-col justify-around">
+          {/* Items */}
+          <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
+            <div className="mb-2.5">
+              <span className="text-3xl text-[#2E562A] font-semibold">49</span>
+              <span className="text-xl text-[#B57C46] ml-2 translate-y-60 float-top">
+                %
+              </span>
+            </div>
+            <div className="text-[#939393] uppercase">
+              <span>Đất cho công cộng, cây xanh và giao thông</span>
+            </div>
           </div>
-          <div className="text-[#939393]">
-            <span>DIỆN TÍCH CÔNG VIÊN</span>
+          <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
+            <div className="mb-2.5">
+              <span className="text-3xl text-[#2E562A] font-semibold">51</span>
+              <span className="text-xl text-[#B57C46] ml-2 translate-y-60 float-top">
+                %
+              </span>
+            </div>
+            <div className="text-[#939393]">
+              <span>MẬT ĐỘ XÂY DỰNG</span>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
-          <div className="mb-2.5">
-            <span className="text-3xl text-[#2E562A] font-semibold">200</span>
+          <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
+            <div className="mb-2.5">
+              <span className="text-3xl text-[#2E562A] font-semibold">
+                6000
+                <sup className="text-xl text-[#B57C46] translate-y-60 float-top">
+                  m2
+                </sup>
+              </span>
+            </div>
+            <div className="text-[#939393] uppercase">
+              <span>DIỆN TÍCH CÔNG VIÊN bốn mùa bên sông</span>
+            </div>
           </div>
-          <div className="text-[#939393]">
-            <span>LOÀI HOA</span>
+          <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
+            <div className="mb-2.5">
+              <span className="text-3xl text-[#2E562A] font-semibold">
+                4 Mùa
+              </span>
+            </div>
+            <div className="text-[#939393] uppercase">
+              <span>Trên một công viên bên sông</span>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
-          <div className="mb-2.5">
-            <span className="text-3xl text-[#2E562A] font-semibold">170</span>
-          </div>
-          <div className="text-[#939393]">
-            <span>CÂY/ NGƯỜI</span>
+          <div className="flex flex-col pl-4 py-4 border-b border-[#939393]">
+            <div className="mb-2.5">
+              <span className="text-3xl text-[#2E562A] font-semibold">
+                16 m
+              </span>
+            </div>
+            <div className="text-[#939393] uppercase">
+              <span>Đường với vỉa hàng tràn</span>
+            </div>
           </div>
         </div>
       </div>
