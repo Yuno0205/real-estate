a5f6a5061407c2a827c66734c1594ea4a26e749f
Yuno0205 <mainhathao195@gmail.com>
2024-11-10 14:43:04 +0700

    update

--- app/components/Q&A/index.tsx
+++ app/components/Q&A/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import city from "@/public/static/images/city.jpg";
 import Image from "next/image";
 import {
@@ -6,7 +7,16 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { useState } from "react";
+
+type AccordionItemValue = "item-1" | "item-2" | "item-3" | "item-4";
 export const QandA = () => {
+  const [openItem, setOpenItem] = useState<AccordionItemValue | null>(null);
+
+  const toggleItem = (item: AccordionItemValue) => {
+    setOpenItem(openItem === item ? null : item);
+  };
+
   return (
     <section className="w-full min-h-screen flex">
       <div className="w-1/2">
@@ -25,25 +35,86 @@ export const QandA = () => {
           câu trả lời (Q&A) mà chúng tôi đã tổng hợp cho Dự án Elite Life
         </span>
         <div className="w-full h-full">
-          <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
+          <Accordion type="single" collapsible className="w-full text-white ">
+            <AccordionItem
+              value="item-1"
+              className={`${
+                openItem === "item-1" ? "bg-[#1C101E]" : "bg-[#241526]"
+              }`}
+              onClick={() => toggleItem("item-1")}
+            >
+              <AccordionTrigger className="pl-12 pr-6 pb-4 pt-8 hover:no-underline leading-8">
+                An ninh trong khu biệt thự được quản lý ra sao khi phân khu này
+                đi vào hoạt động?
+              </AccordionTrigger>
+              <AccordionContent className="text-[#A9C4C3] px-12 pb-10 leading-8">
+                Elite Life là khu nhà phố compound khép kín có kiểm soát An ninh
+                nhiều lớp an ninh, từ ngay tại cổng chỉnh dự án và camera an
+                ninh từng phân khu. Với hệ thống Camera an ninh toàn dự án, và
+                An ninh cơ động sẽ đảm bảo an toàn 24/7 cho Cư dân. Đồng thời,
+                Đơn vị vận hành sẽ phối hợp chặt chẽ và nhanh chóng với các cơ
+                quan ban ngành để đảm bảo trật tựvà sự an lành cho toàn khu.
               </AccordionContent>
             </AccordionItem>
-            <AccordionItem value="item-2">
-              <AccordionTrigger>Is it styled?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It comes with default styles that matches the other
-                components&apos; aesthetic.
+            <AccordionItem
+              value="item-2"
+              className={`${
+                openItem === "item-2" ? "bg-[#1C101E]" : "bg-[#241526]"
+              }`}
+              onClick={() => toggleItem("item-2")}
+            >
+              <AccordionTrigger className="pl-12 pr-6 pb-4 pt-8 hover:no-underline leading-8">
+                Khu biệt thự nhiều cây xanh thì hệ thống đèn chiếu sáng sẽ hoạt
+                động trong thời gian cố định nào, camera bảo vệ đặt ở những đâu?
+                Có mấy chốt bảo vệ?
+              </AccordionTrigger>
+              <AccordionContent className="text-[#A9C4C3] px-12 pb-10 leading-8">
+                Elite Life là khu nhà phố compound khép kín có kiểm soát An ninh
+                nhiều lớp an ninh, từ ngay tại cổng chỉnh dự án và camera an
+                ninh từng phân khu. Với hệ thống Camera an ninh toàn dự án, và
+                An ninh cơ động sẽ đảm bảo an toàn 24/7 cho Cư dân. Đồng thời,
+                Đơn vị vận hành sẽ phối hợp chặt chẽ và nhanh chóng với các cơ
+                quan ban ngành để đảm bảo trật tựvà sự an lành cho toàn khu.
               </AccordionContent>
             </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger>Is it animated?</AccordionTrigger>
-              <AccordionContent>
-                Yes. It's animated by default, but you can disable it if you
-                prefer.
+            <AccordionItem
+              value="item-3"
+              className={`${
+                openItem === "item-3" ? "bg-[#1C101E]" : "bg-[#241526]"
+              }`}
+              onClick={() => toggleItem("item-3")}
+            >
+              <AccordionTrigger className="pl-12 pr-6 pb-4 pt-8 hover:no-underline leading-8">
+                Khu vực Onsen 15.000m2 những ai được sử dụng dịch vụ, Cư dân có
+                được sử dụng hay không? Phí dự kiến nếu Cư dân sử dụng?
+              </AccordionTrigger>
+              <AccordionContent className="text-[#A9C4C3] px-12 pb-10 leading-8">
+                Elite Life là khu nhà phố compound khép kín có kiểm soát An ninh
+                nhiều lớp an ninh, từ ngay tại cổng chỉnh dự án và camera an
+                ninh từng phân khu. Với hệ thống Camera an ninh toàn dự án, và
+                An ninh cơ động sẽ đảm bảo an toàn 24/7 cho Cư dân. Đồng thời,
+                Đơn vị vận hành sẽ phối hợp chặt chẽ và nhanh chóng với các cơ
+                quan ban ngành để đảm bảo trật tựvà sự an lành cho toàn khu.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem
+              value="item-4"
+              className={`${
+                openItem === "item-4" ? "bg-[#1C101E]" : "bg-[#241526]"
+              }`}
+              onClick={() => toggleItem("item-4")}
+            >
+              <AccordionTrigger className="pl-12 pr-6 pb-4 pt-8 hover:no-underline leading-8">
+                Khách hàng có được neo đậu du thuyền cá nhân trước villas hay
+                không?
+              </AccordionTrigger>
+              <AccordionContent className="text-[#A9C4C3] px-12 pb-10 leading-8">
+                Elite Life là khu nhà phố compound khép kín có kiểm soát An ninh
+                nhiều lớp an ninh, từ ngay tại cổng chỉnh dự án và camera an
+                ninh từng phân khu. Với hệ thống Camera an ninh toàn dự án, và
+                An ninh cơ động sẽ đảm bảo an toàn 24/7 cho Cư dân. Đồng thời,
+                Đơn vị vận hành sẽ phối hợp chặt chẽ và nhanh chóng với các cơ
+                quan ban ngành để đảm bảo trật tựvà sự an lành cho toàn khu.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
