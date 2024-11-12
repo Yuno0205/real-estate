c2d97bb275f79da96572c50a70a87f5821a4fb38
Yuno0205 <mainhathao195@gmail.com>
2024-11-12 16:36:35 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       <Extention />
       <Club />
       <Resort />
-      {/* <QandA /> */}
+      <QandA />
       <Showcase />
       {/* <News /> */}
       {/* <Spa />
