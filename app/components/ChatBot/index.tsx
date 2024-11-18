01dacbdc99bef0bf2c580dca593b797fcd144bb9
Yuno0205 <mainhathao195@gmail.com>
2024-11-18 12:34:33 +0700

    test

--- app/components/ChatBot/index.tsx
+++ app/components/ChatBot/index.tsx
@@ -4,14 +4,16 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 function ChatBot() {
   return (
-    <FacebookProvider appId="552946714013365">
-      <CustomChat
-        pageId="462431816958415"
-        minimized
-        loggedInGreeting="Chào bạn! Tôi có thể giúp gì cho bạn?"
-        loggedOutGreeting="Vui lòng đăng nhập Facebook để trò chuyện với chúng tôi."
-      />
-    </FacebookProvider>
+    <div className="w-full min-h-screen">
+      <FacebookProvider appId="552946714013365">
+        <CustomChat
+          pageId="462431816958415"
+          minimized
+          loggedInGreeting="Chào bạn! Tôi có thể giúp gì cho bạn?"
+          loggedOutGreeting="Vui lòng đăng nhập Facebook để trò chuyện với chúng tôi."
+        />
+      </FacebookProvider>
+    </div>
   );
 }
 
