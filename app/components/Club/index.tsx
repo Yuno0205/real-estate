59bf34165edc244c26cab2e84418e70f7ca12f59
Yuno0205 <mainhathao195@gmail.com>
2024-11-19 21:11:10 +0700

    fix small bug

--- app/components/Club/index.tsx
+++ app/components/Club/index.tsx
@@ -1,8 +1,7 @@
 export const Club = () => {
   return (
     <section className="w-full bg-[url('/static/images/green_place.jpg')] bg-cover min-h-[861px] xs:min-h-[400px] flex flex-col justify-between">
-      <div className="max-w-[1140px] mx-auto h-full">
-        
+      <div className="max-w-[1140px] mx-auto h-full"></div>
     </section>
   );
 };
